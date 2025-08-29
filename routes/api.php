60a33da4ae4e65ff06395f82b77a60a515562440
pepefcardoso <pppfcardoso@gmail.com>
2025-08-28 21:50:40 -0300

    fix: fixing api routes import

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\StaffUserProfileController;
 use Illuminate\Http\Request;
